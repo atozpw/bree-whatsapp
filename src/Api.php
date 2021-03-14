6d1532e4a91bc7226f424eed5ea3cc51a0efa21e
Atoz <atozpw@Atoz-Macbook.local>
2021-03-14 16:54:25 +0700

    update send image

--- src/Api.php
+++ src/Api.php
@@ -14,7 +14,7 @@ class Api
         $data = array('number' => $number, 'message' => $message);
 
         $url = $this->host . '/send-message';
-        $ch = curl_init( $url );
+        $ch = curl_init($url);
         curl_setopt($ch, CURLOPT_POST, 1);
         curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
         curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
@@ -25,16 +25,19 @@ class Api
         return $response;
     }
 
-    public function sendMedia($number, $message, $file) {
-        $data = array('number' => $number, 'caption' => $message, 'file' => $file);
+    public function sendImage($number, $message, $file) {
+        $curlfile = new \CURLFILE($file->path(), $file->getClientMimeType(), $file->getClientOriginalName());
+        $data = array('number' => $number, 'caption' => $message, 'file' => $curlfile);
+        $header = array('Content-Type: multipart/form-data');
 
         $url = $this->host . '/send-media';
-        $ch = curl_init( $url );
+        $ch = curl_init($url);
         curl_setopt($ch, CURLOPT_POST, 1);
         curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
         curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_HEADER, 1);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
 
         $response = curl_exec($ch);
         return $response;
