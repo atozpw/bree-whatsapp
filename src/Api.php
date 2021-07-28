a18337db15d66fcbe0e40365031762e177389bae
Atoz <atozpw@Atoz-Macbook.mshome.net>
2021-07-28 08:42:58 +0700

    add function get status

--- src/Api.php
+++ src/Api.php
@@ -5,6 +5,7 @@ namespace Atozpw\BreeWhatsapp;
 class Api
 {
     protected $host;
+    protected $port;
 
     public function __construct() {
 		$this->host = config('bree-whatsapp.bree_whatsapp_host');
@@ -12,26 +13,23 @@ class Api
 	}
     
     public function sendText($number, $message) {
-        $data = array('number' => $number, 'message' => $message);
-
         $url = $this->host . ':' . $this->port . '/send-text';
+        $data = array('number' => $number, 'message' => $message);
         $ch = curl_init($url);
         curl_setopt($ch, CURLOPT_POST, 1);
         curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
         curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
         curl_setopt($ch, CURLOPT_HEADER, 0);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-
         $response = curl_exec($ch);
         return $response;
     }
 
     public function sendImage($number, $message, $file) {
+        $url = $this->host . ':' . $this->port . '/send-image';
         $curlfile = new \CURLFILE($file->path(), $file->getClientMimeType(), $file->getClientOriginalName());
         $data = array('number' => $number, 'caption' => $message, 'file' => $curlfile);
         $header = array('Content-Type: multipart/form-data');
-
-        $url = $this->host . ':' . $this->port . '/send-image';
         $ch = curl_init($url);
         curl_setopt($ch, CURLOPT_POST, 1);
         curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
@@ -39,7 +37,19 @@ class Api
         curl_setopt($ch, CURLOPT_HEADER, 1);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
-
+        $response = curl_exec($ch);
+        return $response;
+    }
+    
+    public function getStatus($id) {
+        $url = $this->host . ':' . $this->port . '/get-status';
+        $data = array('id' => $id);
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         $response = curl_exec($ch);
         return $response;
     }
