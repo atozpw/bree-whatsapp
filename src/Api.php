436ef5e249595512b63f0a34b4a7402ceaa790d9
Atoz <atozpw@Atoz-Macbook.local>
2021-09-08 12:57:31 +0700

    fix response send media

--- src/Api.php
+++ src/Api.php
@@ -34,7 +34,7 @@ class Api
         curl_setopt($ch, CURLOPT_POST, 1);
         curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
         curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-        curl_setopt($ch, CURLOPT_HEADER, 1);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
         $response = curl_exec($ch);
